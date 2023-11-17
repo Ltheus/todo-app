8b8413b5496c1ab581cc344986fbd6b27bcbb31b
MatheusGLopes <matheus.lopes@skyone.solutions>
2023-11-17 12:27:25 -0300

    Created initial visual mock-up

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
 import "./App.css";
+import { FaTrash } from "react-icons/fa";
+
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div>
+        <h1> To-do List </h1>
+      </div>
+      <div>
+        <label htmlFor="item-input">
+          Create a new item
+          <div>
+            <input
+              type="text"
+              name="item-input"
+              id="item-input"
+              placeholder="Type here"
+            />
+          </div>
+        </label>
+      </div>
+      <div>
+        <ul>
+          <li>
+            <input type="checkbox" name="" id="" /> 
+            Item
+            <button> <FaTrash/> </button>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 }
 
 export default App;
